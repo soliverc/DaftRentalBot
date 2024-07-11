3ea56e0ff6ef7645e61d38b47390d19479918f8c
soliverc <soundbeans@gmail.comgit config --global user.email soundbeansgmail.com>
2024-07-11 20:38:17 +0100

    whatever

--- src/main.py
+++ src/main.py
@@ -19,9 +19,11 @@ class SetUp:
         load_dotenv(".env")
         self.SECRET_ID = os.environ.get("secretUser")
         self.SECRET_PASSWORD = os.environ.get("secretPassword")
-        self.SECRET_NAME = os.environ.get("secretName")
-        self.SECRET_CONTACT = os.environ.get("secretContact")
-        self.SECRET_MESSAGE = os.environ.get("secretMessage")
+        self.firstname = os.environ.get("firstname")
+        self.surname = os.environ.get("surname")
+        self.email = os.environ.get("email")
+        self.phone = os.environ.get("phone")
+        self.message = os.environ.get("message")
 
         # Create new csv file to log submissions
         self.file = open("logger.csv", "a")
@@ -77,13 +79,16 @@ class Apply(SetUp):
         self.login()
         self.applied_url = None
         generate_link = GenerateLink()
-        self.link = "https://www.daft.ie/property-for-rent/ireland?rentalPrice_to=1800&location=cork-city&location=cork-city-centre-cork&location=cork-city-suburbs-cork&location=carrigaline-cork&location=ballincollig-cork&location=ovens-cork&numBeds_from=2"
+        self.link = "https://www.daft.ie/property-for-rent/ireland?rentalPrice_to=1800&location=cork-city&location=cork-city-centre-cork&location=cork-city-suburbs-cork&location=carrigaline-cork&location=ballincollig-cork&location=ovens-cork&numBeds_from=2&sort=publishDateDesc"
         self.applicationProcess()
 
         # Run automation for this interval of time
         end_time = generate_end_time()
 
         while time.time() < end_time:
+            print(f"Debug 1: self.link value is {self.link}")
+            print("going to link")
+            sleep(3)
             self.driver.get(self.link)
             sleep(3)
 
@@ -111,8 +116,10 @@ class Apply(SetUp):
             print("Feedback form had popped up! I took care of it.")
 
     def applicationProcess(self):
-        
-        self.driver.get( "https://www.daft.ie/property-for-rent/ireland?rentalPrice_to=1800&location=cork-city&location=cork-city-centre-cork&location=cork-city-suburbs-cork&location=carrigaline-cork&location=ballincollig-cork&location=ovens-cork&numBeds_from=2")
+        print(f"Debug 2: self.link value is {self.link}")
+        print("goign to the link")
+        sleep(3)
+        self.driver.get( "https://www.daft.ie/property-for-rent/ireland?rentalPrice_to=1800&location=cork-city&location=cork-city-centre-cork&location=cork-city-suburbs-cork&location=carrigaline-cork&location=ballincollig-cork&location=ovens-cork&numBeds_from=2&sort=publishDateDesc")
         sleep(3)
         self.checkFeedback()
 
@@ -126,15 +133,22 @@ class Apply(SetUp):
         sleep(3)
         self.applied_url = self.driver.current_url
         
-        # old '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div/div[2]/h1'
+        # save the address
         log_address = self.driver.find_element(
-            By.XPATH, '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div/div[2]/div[1]/h1'
+            By.XPATH, '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div/div[2]/h1'
         ).text
+
         print(log_address)
+        if "student" in log_address.lower():
+            print("Skipping student accommodation")
+            return  # Skip this property
+        
+        # save price
         log_price = self.driver.find_element(
             By.XPATH,
-            '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div/div[2]/div[1]/span',
+            '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div/div[2]/div[1]/h2',
         ).text
+        print("Price: ", log_price)
         log_price = "EUR " + log_price[1:]
         log_entry = [log_price, log_address, self.applied_url, "Applied"]
 
@@ -148,33 +162,58 @@ class Apply(SetUp):
         ).click()
         sleep(3)
         self.checkFeedback()
+
+        # use this to check if this has already been applied for
+        already_applied_text = '//*[@id="contact-form-modal"]/div[2]/div[3]/div/div/span/svg'
+
         # Enter First Name
         self.driver.find_element(By.XPATH, '//*[@id="keyword1"]').send_keys(
-            self.SECRET_NAME
+            self.firstname
         )
-        self.checkFeedback()
-        # Enter Email-ID
+
+        # Surname
         self.driver.find_element(By.XPATH, '//*[@id="keyword2"]').send_keys(
-            self.SECRET_ID
+            self.surname
         )
         self.checkFeedback()
-        # Enter Contact Number
+
+        # Enter Email-ID
         self.driver.find_element(By.XPATH, '//*[@id="keyword3"]').send_keys(
-            self.SECRET_CONTACT
+            self.email
+        )
+        self.checkFeedback()
+
+        # Enter Contact Number
+        self.driver.find_element(By.XPATH, '//*[@id="keyword4"]').send_keys(
+            self.phone
         )
         self.checkFeedback()
         # Enter Application text
         self.driver.find_element(By.XPATH, '//*[@id="message"]').send_keys(
-            self.SECRET_MESSAGE
+            self.message
         )
 
         self.checkFeedback()
 
         sleep(5)
+        # add number of people
+        plusOne = '//*[@id="input-wrapper-adultTenants"]/div/div/div/button[2]'
+        self.driver.find_element(
+            By.XPATH, plusOne
+        ).click()
+        sleep(2)
+
+        # specify no pets
+        noPets = '//*[@id="hasPets-item-1"]'
+        # Click no pets
+        self.driver.find_element(
+            By.XPATH, noPets
+        ).click()
+        sleep(2)
 
         # Send the applicaiton
         self.driver.find_element(
-            By.XPATH, '//*[@id="contact-form-modal"]/div[2]/form/div/div[5]/div/button'
+            By.XPATH, '//*[@id="contact-form-modal"]/div[2]/form/div/div[9]/div/button/div/span'
         ).click()
         sleep(2)
 
